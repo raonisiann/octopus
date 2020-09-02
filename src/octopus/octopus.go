98a4cb1c340ad9a2613e678c9a2bf7cc4ecaa1df
Raoni Sian <raonisiann@live.com>
2020-09-02 01:31:00 +0100

    Adding basic help text. Not all thing defined in help exists, but they will be used to store ideas :D

--- src/octopus/octopus.go
+++ src/octopus/octopus.go
@@ -9,7 +9,7 @@ func main() {
 	fmt.Println("Starting with Go")
 
 	if len(os.Args) < 2 {
-		fmt.Printf("Show help")
+		showGlobalHelp()
 		os.Exit(-1)
 	}
 
@@ -18,11 +18,36 @@ func main() {
 		fmt.Println("Apply sub-command")
 		ApplyCmd(os.Args[2:])
 	case "help":
-		fmt.Println("Show help")
+		showGlobalHelp()
 	case "validate":
 		fmt.Println("Validate sub-command")
 	default:
 		fmt.Printf("Unknown option '%s'\n", cmd)
+		showGlobalHelp()
 	}
 
+}
+
+func showGlobalHelp() {
+	helpText := `
+Octopus vX.X.X
+
+Basic description about what is octopus and what 
+can be done with it. Should be no more than 4 lines. 
+
+Online documentation at: https://github.com/raonisiann/octopus
+
+Main Commands:
+    apply              Apply resource to target
+    config             Configure parameters on agent
+    get                Get catalog from Octopus server (requires server setup)
+    help               Show this help
+    import             Import existing resource into class (requires connectivity with target)
+    plan               Generated differences between current state and new state
+    pull               Pull catalog from remote Octopus repository
+    validate           Validate catalog file
+    version            Show agent version
+
+`
+	fmt.Println(helpText)
 }
